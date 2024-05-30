8a2fe4eacd15e63014575be0763523f248922ac8
agarccav <andres.garcia.cavero@nttdata.com>
2024-05-30 10:48:43 +0200

    webpack trabajar en ui.frontend y reload

--- ui.frontend/src/main/webpack/components/_andres.js
+++ ui.frontend/src/main/webpack/components/_andres.js
@@ -1,1 +1,26 @@
-console.log("andres.js en webpack");
+
+(function(){
+
+    function ready(fn) {
+        if (document.readyState !== 'loading') {
+          fn();
+        } else {
+          document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+    function init(){
+
+        document.querySelectorAll('.cmp-andres').forEach((instance)=>{
+
+            instance.dataset.initiated = true;
+
+        });
+    }
+
+    ready(init());
+
+})();
+
+
+console.log('andres.js but using local version');
